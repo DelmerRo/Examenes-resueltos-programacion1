0b7ffa3b2cf3146474f937148af8a5a2d8bfbc04
DelmerRo <jindrg@gmail.com>
2021-09-17 08:26:04 -0300

    Se mejoro esteticamente

--- src/ort/edu/ar/parciallicenciadeconducir/Auto.java
+++ src/ort/edu/ar/parciallicenciadeconducir/Auto.java
@@ -1,20 +1,16 @@
 package ort.edu.ar.parciallicenciadeconducir;
 
-public class Auto extends Vehiculo{
+public class Auto extends Vehiculo {
 	private int cantPuertas;
 
 	public Auto(String patente, String marca, double cilindrada, int cantPuertas) {
 		super(patente, marca, cilindrada);
-		this.cantPuertas=cantPuertas;
+		this.cantPuertas = cantPuertas;
 	}
 
 	@Override
 	public String toString() {
 		return "Auto [cantPuertas=" + cantPuertas + "]";
 	}
-
-	
-		
-
 
 }
